01af769dc31f89398170c6b08c4e83d8c2a1cff1
Mrkubsonallo <jakubek.c25@gmail.com>
2022-12-16 11:39:13 +0100

    elementy dostaja onclick do odpowiedniego okienka

--- kalendarz_adwentowy/script.js
+++ kalendarz_adwentowy/script.js
@@ -2,28 +2,39 @@ window.onload = calendar_build()
 
 function calendar_build()
 {
+    let date = new Date
+    let month = date.getDate()
+
     let num = 1
     const calendar = document.getElementById('calendar')
     for(let i=1; i<25; i++)
     {
         if(i>=1 && i<=4)
-            calendar.innerHTML += '<div class="row1" id="field'+i+'" onclick="clicked(this)">'+i+'</div>'
+            calendar.innerHTML += '<div class="row1" id="field'+i+'">'+i+'</div>'
         else if(i>=5 && i<=8)
-            calendar.innerHTML += '<div class="row2" id="field'+i+'" onclick="clicked(this)">'+i+'</div>'
+            calendar.innerHTML += '<div class="row2" id="field'+i+'">'+i+'</div>'
         else if(i>=9 && i<=12 )
-            calendar.innerHTML += '<div class="row3" id="field'+i+'" onclick="clicked(this)">'+i+'</div>'
+            calendar.innerHTML += '<div class="row3" id="field'+i+'">'+i+'</div>'
         else if(i>=13 && i<=16)
-            calendar.innerHTML += '<div class="row4" id="field'+i+'" onclick="clicked(this)">'+i+'</div>'
+            calendar.innerHTML += '<div class="row4" id="field'+i+'">'+i+'</div>'
         else if(i>=17 && i<=20)
-            calendar.innerHTML += '<div class="row5" id="field'+i+'" onclick="clicked(this)">'+i+'</div>'
+            calendar.innerHTML += '<div class="row5" id="field'+i+'">'+i+'</div>'
         else if(i>=21 && i<=24)
-            calendar.innerHTML += '<div class="row6" id="field'+i+'" onclick="clicked(this)">'+i+'</div>'
+            calendar.innerHTML += '<div class="row6" id="field'+i+'">'+i+'</div>'
+    }
+
+    for(i=1; i<=month; i++)
+    {   
+        if(i<=month)
+        {
+            let element = document.getElementsByTagName('div')[i]
+            element.setAttribute('onclick', 'clicked(this)')
+            console.log(i)
+        }
     }
 }
 
 function clicked(element)
 {
-    let id_vla = element.getAttribute('id')
-    let date = new Date
-    let month = date.getDate()
+    console.log(element.getAttribute('id'))
 }
